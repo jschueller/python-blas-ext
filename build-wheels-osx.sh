1fbd2173637649569ff0a4413bf996773ef04580
Julien Schueller <schueller@phimeca.com>
2019-01-18 22:45:55 +0100

    fix

--- build-wheels-osx.sh
+++ build-wheels-osx.sh
@@ -16,7 +16,6 @@ python setup.py build
 python setup.py install
 
 cd ${HOME}/miniconda/lib/python${PYVERD}/site-packages/
-ls -l
 
 # write metadata
 mkdir myModule-${VERSION}.dist-info
@@ -24,18 +23,20 @@ touch myModule-${VERSION}.dist-info/WHEEL
 touch myModule-${VERSION}.dist-info/METADATA
 
 # create archive
-zip -r myModule-${VERSION}-${TAG}.whl myModule myModule-${VERSION}.dist-info
+zip -r /tmp/myModule-${VERSION}-${TAG}.whl myModule myModule-${VERSION}.dist-info
 
-delocate-listdeps myModule-${VERSION}-${TAG}.whl
-delocate-wheel myModule-${VERSION}-${TAG}.whl
-delocate-listdeps --all myModule-${VERSION}-${TAG}.whl
+delocate-listdeps /tmp/myModule-${VERSION}-${TAG}.whl
+delocate-wheel /tmp/myModule-${VERSION}-${TAG}.whl
+delocate-listdeps --all /tmp/myModule-${VERSION}-${TAG}.whl
+
+otool -l ${HOME}/miniconda/lib/libopenblas.dylib
 
 # missing libs
 mkdir myModule/.dylibs
 for libname in libquadmath.0 libgcc_s.1 libc++abi.1 libgfortran.3
 do
   cp ${HOME}/miniconda/lib/${libname}.dylib myModule/.dylibs
-  zip -u myModule-${VERSION}-${TAG}.whl myModule/.dylibs/${libname}.dylib
+  zip -u /tmp/myModule-${VERSION}-${TAG}.whl myModule/.dylibs/${libname}.dylib
 done
 
 cd
@@ -43,7 +44,7 @@ rm -r ${HOME}/miniconda
 bash /tmp/Miniconda3-latest-MacOSX-x86_64.sh -b -p ${HOME}/miniconda
 conda install -y python=${PYVERD} pip
 
-pip install myModule --no-index -f ${TRAVIS_BUILD_DIR}/wheelhouse
+pip install myModule --no-index -f /tmp/
 python -c "import myModule; myModule.helloworld()"
 
 
